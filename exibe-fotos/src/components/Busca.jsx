c781904e35646217cfd23885262827e75cb7784c
Lucas Crapino <lucascrapino@gmail.com>
2025-04-05 12:50:54 -0300

    learn(07-react): encerra a pagina 8
    - classes de manipulacao de borda parecem nao funcionar
    - retomar falando sobre componentes controlados e nao controlados

---
+++ exibe-fotos/src/components/Busca.jsx
@@ -0,0 +1,31 @@
+// rcc
+import React, { Component } from 'react'
+import { Button } from 'primereact/Button'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
+
+export default class Busca extends Component {
+
+    state = {
+        termoDeBusca: ''
+    }
+  render() {
+    return (
+      <div className='flex flex-column'>
+        <IconField iconPosition='left'>
+            <InputIcon className='pi pi-search'/>
+            <InputText className='w-full' placeholder={this.props.dica}/>
+        </IconField>
+        <Button
+        className='mt-1' 
+        label='OK' 
+        outlined/>
+      </div>
+    )
+  }
+}
+
+Busca.defaultProps = {
+    dica: 'Buscar...'
+}
